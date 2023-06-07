3a9c4b511f8fb65385d59390ce8a34c6695a560e
tanglz <lizhen.tang@wealthsimple.com>
2023-06-07 10:39:45 -0400

    result save to csv file

--- src/app.ts
+++ src/app.ts
@@ -6,27 +6,31 @@ import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import {parseContentByPlaywright, parseContentByPostlight} from "./parseContent.js";
 import {fetchSecurityNews} from "./securityNews.js";
 import {convertToCSV, CSVRow, insertRow, saveCSVToFile} from "./saveResult.js";
+import path from 'path';
 
 const run = async () => {
     // Test parameters
     const securityId = "sec-s-2875ffa767cf4185bdd3a096726670c6";
-    const from = "2023-06-06";
+    const from = "2023-06-07";
     // model optimization
     const modelName= "gpt-3.5-turbo";
     const chunkSize= 2000;
-    const prompt= "Please summarize the following text in 3 bullet points:";
+    const conentPrompt= "Summarize the following article in a style of a tweet and do not exceed the 200 characters limit:";
+    const titlePrompt= "Create a title that is short and to the point about the article:";
 
     // fetch security news from graphql endpoint, return urls of news
     const resultData: CSVRow[]=[];
     const securityNews=await fetchSecurityNews(securityId, from);
-    const urls=securityNews.map((news:any)=>news.url);
-     for(const url of urls){
-        const content=await parseContentByPostlight(url);
+    let urls=securityNews.map((news:any)=>news.url);
+    // urls=urls.slice(0, 1);
+    for(const url of urls){
+        const result=await parseContentByPostlight(url);
         const textSplitter = new RecursiveCharacterTextSplitter({chunkSize: chunkSize});
         // @ts-ignore
-        const splittedDocs = await textSplitter.createDocuments([content]);
+        const splittedDocs = await textSplitter.createDocuments([result.content]);
         const model = new OpenAI({
             modelName: modelName,
+            temperature: 0.1,
             openAIApiKey: process.env.OPENAI_API_KEY,
         });
         const chain = loadSummarizationChain(model, {type: "map_reduce"});
@@ -34,18 +38,16 @@ const run = async () => {
             input_documents: splittedDocs,
         });
         const sum = await model.call(
-            prompt + ` ${res.text} `
+            conentPrompt + ` ${res.text} `
         );
-        console.log(url);
-        console.log(sum);
-        insertRow(resultData, url, sum);
+        const tt = await model.call(
+            titlePrompt + ` ${res.text} `
+        );
+        insertRow(resultData, url, sum, result.title, tt);
     };
-
     // Convert data to CSV format
     const csvData = convertToCSV(resultData);
-
     // Save CSV data to a file
-    const filePath = 'result_files/output.csv';
-    saveCSVToFile(csvData, filePath);
+    saveCSVToFile(csvData, 'output_APPL.csv');
 }
 run();
